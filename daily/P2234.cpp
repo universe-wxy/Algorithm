6a22c8b8fcd010d618ba823b25315f4899da55e4
王熙宇 <2537239220@qq.com>
2022-12-19 20:44:17 +0800

    完成P2234

--- daily/P2234.cpp
+++ daily/P2234.cpp
@@ -2,54 +2,37 @@
 
 using namespace std;
 
-int vis[1000005];
+int a[100005];
 
 int main()
 {
-    //freopen("in.txt","r",stdin);
-    //freopen("out.txt","w",stdout);
-    vector<int> days;
+    // freopen("in.txt","r",stdin);
+    // freopen("out.txt","w",stdout);
     int n;
-    int cnt;
+    int cnt=0,temp;
     cin>>n;
-    memset(vis,0,sizeof(vis));
     for(int i=0;i<n;i++){
-        int temp;
         cin>>temp;
-        days.push_back(temp);
-        vis[temp]++;
         if(i==0){
-        	cnt=temp;
-        	continue;
-		}
-        if(vis[temp]>=2){
-            cnt+=0;
+            cnt+=temp;
+            a[i]=temp;
             continue;
         }
-        for(int j=1;j<1000005;j++){
-            if(temp+j>=1000005&&temp-j<0)
-                continue;
-            if(temp+j<1000005&&temp-j>=0){
-                if(vis[temp+j]||vis[temp-j]){
-                    cnt+=j;
-                    break;
-                }
-            }
-            else if(temp+j>1000005){
-                if(vis[temp-j]){
-                    cnt+=j;
-                    break;
-                }
-            }
-            else if(vis[temp+j]){
-                    cnt+=j;
-                    break;
-                }
+        int j=i-1;
+        while(j>=0&&a[j]>temp){
+            a[j+1]=a[j];
+            j--;
         }
+        a[++j]=temp;
+        if(j==0)
+            cnt+=abs(a[1]-temp);
+        else if(j==i)
+            cnt+=abs(a[i-1]-temp);
+        else
+            cnt+=min(abs(a[j+1]-temp),abs(a[j-1]-temp));
     }
-    
     cout<<cnt<<endl;
-    //fclose(stdin);
-    //fclose(stdout);
+    // fclose(stdin);
+    // fclose(stdout);
     return 0;
 }
